cf9889335e870ee30809e6754818a39eb2bb373a
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2025-12-02 11:20:17 +0530

    Time: 0 ms (100%), Space: 42.2 MB (10.09%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int tribonacci(int n) {
+            
+            int[] res=new int[n+1];
+            Arrays.fill(res,-1);
+            return check(n,res);
+    }
+    public int check(int i,int[] res){
+        if (i==0)
+            return res[i]=0;
+        if(i<=2)
+            return res[i]=1;
+        if (res[i]!=-1) 
+            return res[i];
+        int a=check(i-1,res);
+        int b=check(i-2,res);
+        int c=check(i-3,res);
+
+        return res[i]=a+b+c;
+    }
+}
