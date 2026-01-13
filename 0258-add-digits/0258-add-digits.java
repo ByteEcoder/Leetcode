938ddd148ba42d463ac0e3f750706815ec40b50a
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-01-14 00:44:48 +0530

    Time: 1 ms (97.69%), Space: 42.9 MB (13.76%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -2,21 +2,17 @@ class Solution {
     public int addDigits(int num) {
         
         
-        int res=0;
-        while(num>0)
+        
+        while(num>9)
         {
-
+        int res=0;
+           while(num>0) {
             res+=num%10;
             num=num/10;
-
-            while(res>9)
-            {
-                int a=res%10;
-                int b=res/10;
-                res=a+b;
             }
+            num=res;
         }
 
-        return res;
+        return num;
     }
 }
