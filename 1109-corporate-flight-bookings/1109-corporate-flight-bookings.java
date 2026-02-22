192d35c73dca323d0f931273db02c50729eed456
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-02-22 15:56:41 +0530

    Time: 1108 ms (7.06%), Space: 84.8 MB (43.82%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        
+        int res[]=new int[n];
+        for(int i=0;i<bookings.length;i++)
+        {
+            int first=bookings[i][0];
+            int last=bookings[i][1];
+            int seat=bookings[i][2];
+
+            res[first-1]+=bookings[i][2];
+            while(last>first)
+                {res[last-1]+=bookings[i][2];
+                last--;
+                }
+        }
+        return res;
+    }
+}
