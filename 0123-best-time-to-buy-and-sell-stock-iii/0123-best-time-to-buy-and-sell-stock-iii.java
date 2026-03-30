4dfd052a487b4c247cdeec2115989401a46af361
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-03-30 09:51:26 +0530

    Time: 115 ms (17.76%), Space: 117.7 MB (34.21%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -19,21 +19,19 @@ class Solution {
         if(dp[i][state][tr]!=Integer.MIN_VALUE)
             return dp[i][state][tr];
 
-        int buy=0;
-        int sell=0;
+        int pick=0;
+        int nt=0;
         if(state==0)
         {
-            int a=-prices[i]+help(i+1,1,tr,dp,prices);
-            int b=help(i+1,0,tr,dp,prices);
-            buy=Math.max(a,b);
+            pick=-prices[i]+help(i+1,1,tr,dp,prices);
+            nt=help(i+1,0,tr,dp,prices);
         }
         if(state!=0)
         {
-            int a=prices[i]+help(i+1,0,tr+1,dp,prices);
-            int b=help(i+1,1,tr,dp,prices);
-            sell=Math.max(a,b);
+            pick=prices[i]+help(i+1,0,tr+1,dp,prices);
+            nt=help(i+1,1,tr,dp,prices);
         }
 
-        return dp[i][state][tr]=Math.max(buy,sell);
+        return dp[i][state][tr]=Math.max(pick,nt);
     }
 }
