bfccc8767c21119236a7b5be46cf4c021803cce2
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-04-27 09:22:08 +0530

    Time: 5 ms (9.94%), Space: 47.6 MB (23.5%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,19 +1,22 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-
+        int n=nums.length;
+        int rsum=0;
+        int lsum=0;
         int sum=Arrays.stream(nums).sum();
-        int lsum=0,rsum=0;
-        for(int i=0;i<nums.length;i++)
+        int i=0;
+        while(i<n)
         {
-           rsum=sum-lsum-nums[i];
-
-           if(rsum==lsum)
-            return i;
-
+            rsum=sum-lsum-nums[i];
+            if(rsum==lsum)
+            {
+                return i;
+            }
             lsum+=nums[i];
+
+            i++;
         }
 
         return -1;
-
     }
 }
