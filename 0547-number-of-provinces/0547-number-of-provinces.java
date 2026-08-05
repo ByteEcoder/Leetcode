f87a5e990a6fddcdb3731393ddb00273e48e8490
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-08-05 23:04:11 +0530

    Time: 1 ms (86.5%), Space: 47.4 MB (31.76%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,56 +1,73 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        
-        List<List<Integer>> graph=new ArrayList<>();
-        int n=isConnected.length;
-        for(int i=0;i<n;i++)
-        {
-            graph.add(new ArrayList<>());
-        }
-
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(isConnected[i][j]==1&&  i!=j)
-                {
-                    graph.get(i).add(j);
-                }
-            }
-        }
-        boolean visit[]=new boolean[n];
-        int count=0;
-
-        for(int i=0;i<n;i++)
-        {
-            if(!visit[i])
-            {
-                bfs(visit,graph,i);
-                count++;
-            }
-        }
-        return count;
-        
-    }
-
-    public void bfs(boolean visit[],List<List<Integer>> graph,int start)
+    class DSU
     {
-        Queue<Integer> q=new LinkedList<>();
-        q.add(start);
-        visit[start] = true;
-
-        while(!q.isEmpty())
+        int[] parent;
+        int[] rank;
+        DSU(int n)
         {
-            int node=q.poll();
-            for(int neigh:graph.get(node))
+            parent = new int[n];
+            rank = new int[n];
+            for(int i = 0; i < n; i++)
             {
-                if(!visit[neigh])
+                parent[i] = i;
+            }
+        }
+
+        int find(int x)
+        {
+            if(parent[x] != x)
+            {
+                parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+
+        void union(int a, int b)
+        {
+            int pa  = find(a);
+            int pb = find(b);
+            if(pa == pb) return;
+            if(rank[pa] < rank[pb])
+            {
+                parent[pa] = pb;
+            }
+            else if(rank[pa] > rank[pb]) parent[pb] = pa;
+            else 
+            {
+                parent[pb] = pa;
+                rank[pa]++;
+            }    
+        }
+    }
+    public int findCircleNum(int[][] isConnected)
+    {
+        int n = isConnected.length;
+        int m = isConnected[0].length;
+
+        DSU dsu = new DSU(n);  
+        int cnt = 0; 
+
+        for(int i = 0 ; i < n; i++)
+        {
+            for(int j = i+1; j < m; j++)
+            {
+                if(isConnected[i][j] == 1)
                 {
-                    visit[neigh]=true;
-                    q.add(neigh);
+                    dsu.union(i,j);
                 }
             }
         }
-        
+
+        for(int i = 0; i < n; i++)
+        {
+            if(dsu.find(i) == i)
+            {
+                cnt++;
+            }
+        }
+
+        return cnt;
+
+
     }
 }
