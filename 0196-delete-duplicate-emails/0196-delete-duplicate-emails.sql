c1665699a0a79efdbef75982cdb3907a81f19986
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-09-02 00:18:55 +0530

    Time: 850 ms (73.88%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
 delete c1
-from Person c1
-join Person c2
-on c1.email = c2.email
-and c1.id>c2.id;
+from Person c1, Person c2
+where c1.email=c2.email and c1.id>c2.id;
 
 
