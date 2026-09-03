40085d24336851738476b4931f7d90021ca30e9b
Sumit Kumar Verma <sumit.23scse1410063@galgotiasuniversity.ac.in>
2026-09-03 23:08:22 +0530

    Time: 5 ms (83.33%), Space: 121.9 MB (18.12%) - LeetHub

---
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        boolean hasOdd = false;
+        boolean hasEven = false;
+        int min = Integer.MAX_VALUE;
+
+        for (int num : nums1) {
+            min = Math.min(min, num);
+
+            if (num % 2 == 0)
+                hasEven = true;
+            else
+                hasOdd = true;
+        }
+
+        if (!hasOdd || !hasEven)
+            return true;
+
+        return min % 2 != 0;
+    }
+}
